4733d65a184d1c26ca0d9bb9d5c2dc6edaf0fb54
Giovana Guaita Primon <giovanaguaita@outlook.com>
2024-01-09 11:13:05 -0300

    Edição tela Controle de Gastos (ORÇAMENTO)

--- src/pages/Notification/index.js
+++ src/pages/Notification/index.js
@@ -1,22 +1,80 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-export default function Notification(){
-    return(
-        <View style={styles.container}>
-            <Text style={StyleSheet.text}>Pagina Notification</Text>
-        </View>
-    );
+export default function Notification({ navigation }) {
+  const navigateToCadastro = (screenName) => {
+    navigation.navigate(screenName);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Controle de Gastos</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigateToCadastro('Casa')}
+      >
+        <Text style={styles.buttonText}>Casa</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigateToCadastro('Alimentacao')}
+      >
+        <Text style={styles.buttonText}>Alimentação</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigateToCadastro('Transporte')}
+      >
+        <Text style={styles.buttonText}>Transporte</Text>
+      </TouchableOpacity> 
+     
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigateToCadastro('SaudeBeleza')}
+      >
+        <Text style={styles.buttonText}>Saúde & Beleza</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigateToCadastro('Educacao')}
+      >
+        <Text style={styles.buttonText}>Educação</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigateToCadastro('Lazer')}
+      >
+        <Text style={styles.buttonText}>Lazer & Extras</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text:{
-        fontSize: 25,
-        fontWeight: 'bold'
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });
